2f5ec60919bce580afce9e5bc84d43162e4f9d91
wxyhhh <weixy@zju.edu.cn>
2018-06-05 00:33:08 +0800

    The first leetcode training

---
+++ TwoSum/Solution.h
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for(int i = 0; i < nums.size();i++){
+            for(int j = 0; j!=nums.size();j++){
+                if(j == i){
+                    continue;
+                }
+                if (nums[i] + nums[j] == target){
+                    result.push_back(i);
+                    result.push_back(j);
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+};
